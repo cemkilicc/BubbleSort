379e7a48129e3db8c7cf883b155cef3cf5c002c4
Cem KILIÇ <cem.kilicm@outlook.com>
2021-02-26 17:49:10 +0300

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 11, 93, 45, 98, 13, 55 };
+
+            BubbleSort(arr);
+        }
+
+        public static void BubbleSort(int[] myArray)
+        {
+            int temp;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                bool ordered = true;
+
+                for (int j = myArray.Length-1; j > i; j--)
+                {
+                    if (myArray[j-1] > myArray[j])
+                    {
+                        ordered = false;
+
+                        temp = myArray[j - 1];
+                        myArray[j - 1] = myArray[j];
+                        myArray[j] = temp;
+                    }
+                }
+                if (ordered)
+                    break;
+            }
+
+            for ( int k = 0; k < myArray.Length; k++)
+            {
+                if (k == myArray.Length-1)
+                    Console.Write(myArray[k]);
+                else
+                    Console.Write(myArray[k] + ", ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
